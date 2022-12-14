f13009e7afc558937f16ab69cb622c6d70578f51
malte.horst <maltehorst@gmail.com>
2022-12-14 19:35:34 +0100

    add proxy for requests

--- mining/utils.py
+++ mining/utils.py
@@ -1,6 +1,4 @@
 import csv
-import random
-import urllib
 import json
 
 full_station_file = []
@@ -22,20 +20,6 @@ def station_id_to_name(station_id: str) -> str:
     for line in full_station_file:
         if line[3] == station_id:
             return line[0]
-
-
-def get_proxy_list() -> list[str]:
-    for line in urllib.request.urlopen(
-        "https://raw.githubusercontent.com/shiftytr/proxy-list/master/https.txt"
-    ):
-        proxy_list.append(line)
-    return proxy_list
-
-
-def get_random_proxy() -> dict:
-    raw = random.choice(proxy_list)
-    return {"https": raw}
-
 
 if __name__ == "__main__":
     tmp = read_station_ids_csv("vvs_sbahn_haltestellen_2022.csv")
