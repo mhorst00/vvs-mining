25c430805accda58657859eda9bad0fcb22fb816
mhorst00 <maltehorst@gmail.com>
2023-02-09 12:57:42 +0100

    final deployment for mining

--- mining/utils.py
+++ mining/utils.py
@@ -22,15 +22,3 @@ def station_id_to_name(station_id: str) -> str:
         if line[3] == station_id:
             return line[0]
 
-
-def get_proxy_list() -> list[str]:
-    for line in urllib.request.urlopen(
-        "https://raw.githubusercontent.com/shiftytr/proxy-list/master/https.txt"
-    ):
-        proxy_list.append(line)
-    return proxy_list
-
-
-def get_random_proxy() -> dict:
-    raw = random.choice(proxy_list)
-    return {"https": raw}
