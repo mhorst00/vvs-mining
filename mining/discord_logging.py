3136fbef13ced73449fa2d446f80b6d43df63d75
Yannis Plaschko <yannis.plaschko@breuninger.de>
2023-06-09 18:47:02 +0200

    improve discord logging to send only one message in one channel

--- mining/discord_logging.py
+++ mining/discord_logging.py
@@ -5,8 +5,9 @@ from datetime import datetime
 from pathlib import Path
 
 infoCount = 0
-errorCount = 0
 warningCount = 0
+errorCount = 0
+
 
 # webhook.send("<@&1020311126313009233> Hello there!") #Mention Role
 # webhook.send("<#1020308171140628480> Hello there!") #Mention Channel
@@ -17,23 +18,15 @@ WEBHOOK_LOGGING_URL = os.environ.get(
     "WEBHOOK_LOGGING_URL",
     "",
 )
-WEBHOOK_ERROR_URL = os.environ.get(
-    "WEBHOOK_ERROR_URL",
-    "",
-)
 
-WEBHOOK_ERROR_ENABLED = True
 WEBHOOK_LOGGING_ENABLED = True
 
-WEBHOOK_ERROR: SyncWebhook
 WEBHOOK_LOGGING: SyncWebhook
 
 LOG_FILENAME = f'/data/logs/mining_log {str(datetime.now()).replace(" ", "_")}.log'
 
 
 def initialise():
-    global WEBHOOK_ERROR
-    global WEBHOOK_ERROR_ENABLED
     global WEBHOOK_LOGGING_ENABLED
     global WEBHOOK_LOGGING
     global LOG_FILENAME
@@ -47,15 +40,6 @@ def initialise():
         level=logging.INFO,
     )
     logger = logging.getLogger("mining_logger")
-    # if url is not present disable the logging
-    if WEBHOOK_ERROR_URL == "":
-        logger.warning("WEBHOOK_ERROR_URL is not set, deactivating error logging")
-        WEBHOOK_ERROR_ENABLED = False
-    else:
-        try:
-            WEBHOOK_ERROR = SyncWebhook.from_url(WEBHOOK_ERROR_URL)
-        except Exception as err:
-            logger.warning(err)
 
     if WEBHOOK_LOGGING_URL == "":
         logger.warning("WEBHOOK_LOGGING_URL is not set, deactivating info logging")
@@ -67,21 +51,24 @@ def initialise():
             logger.warning(err)
 
 
-def finishLogging(numberOfTrips: int, numberOfBytes: int):
+def finishLogging(numberOfDepartures: int):
     global LOG_FILENAME
     global WEBHOOK_LOGGING
     global WEBHOOK_LOGGING_ENABLED
-    global WEBHOOK_ERROR
-    global WEBHOOK_ERROR_ENABLED
     global errorCount
     global warningCount
     global infoCount
     # send basic information about result
     if WEBHOOK_LOGGING_ENABLED:
-        WEBHOOK_LOGGING.send("Import has finished")
-        WEBHOOK_LOGGING.send("Number of trips: " + str(numberOfTrips))
-        WEBHOOK_LOGGING.send("Size in bytes: " + str(numberOfBytes))
-        message = f"There were {warningCount} Warnings and {infoCount} Infos"
+        messageList = ["Import has finished\n"]
+        if errorCount != 0:
+            messageList.extend(
+                f"<@&1020311126313009233> There were {errorCount} Errors! \n"
+            )
+        messageList.extend("Number of Departures: " + str(numberOfDepartures) + "\n")
+        messageList.extend(f"There were {warningCount} Warnings and {infoCount} Infos")
+        message = "".join(messageList)
+
         # send log file
         try:
             with open(file=LOG_FILENAME, mode="rb") as f:
@@ -90,10 +77,6 @@ def finishLogging(numberOfTrips: int, numberOfBytes: int):
             logger = logging.getLogger("mining_logger")
             logger.warning(err)
         WEBHOOK_LOGGING.send(message, file=file)
-
-    if WEBHOOK_ERROR_ENABLED:
-        message = f"There were {errorCount} Errors"
-        WEBHOOK_ERROR.send(message)
 
 
 def info(message):
@@ -111,12 +94,7 @@ def warning(message):
 
 
 def error(message):
-    global WEBHOOK_ERROR
     global errorCount
     logger = logging.getLogger("mining_logger")
-    logger.exception(message)
+    logger.error(message)
     errorCount += 1
-    if WEBHOOK_ERROR_ENABLED:
-        WEBHOOK_ERROR.send(
-            "<@&1020311126313009233> " + "ERROR: " + str(message),
-        )
