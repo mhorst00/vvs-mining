ccab4795e8893b637b108971eba078ac027da20e
maltehorst <maltehorst@gmail.com>
2024-12-19 23:23:11 +0100

    connect to db

--- web/src/http/station.rs
+++ web/src/http/station.rs
@@ -1,16 +1,27 @@
 use askama::Template;
-use axum::{routing::get, Router};
+use axum::{routing::get, Extension, Router};
+use sqlx::PgPool;
 
 #[derive(Template)]
 #[template(path = "station.html")]
 struct StationTemplate {
-    stations: Vec<(String, f32)>,
+    stations: Vec<StationDelay>,
 }
 
-async fn station_delay() -> StationTemplate {
-    StationTemplate {
-        stations: vec![("test".to_string(), 0.3)],
-    }
+#[derive(sqlx::FromRow)]
+pub struct StationDelay {
+    pub stop_name: String,
+    pub delay: f32,
+}
+
+async fn station_delay(Extension(pool): Extension<PgPool>) -> StationTemplate {
+    let sql = "SELECT stop_name, avg(delay)::real as delay FROM station_delay GROUP BY stop_name ORDER BY delay DESC LIMIT 5"
+        .to_string();
+    let stations = sqlx::query_as::<_, StationDelay>(&sql)
+        .fetch_all(&pool)
+        .await
+        .unwrap();
+    StationTemplate { stations }
 }
 
 pub(crate) fn router() -> Router {
